09c3fa4873c3b1e021a1063dd69d6d5623a0d8ca
Kaif Khan <kaifkhan02170@gmail.com>
2025-10-18 13:09:34 +0530

    Add files via upload

---
+++ Jarvis/musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "lose my mind": "https://www.youtube.com/watch?v=WWEs82u37Mw&list=RDWWEs82u37Mw&start_radio=1&pp=ygUMbG9zZSBteSBtaW5koAcB",
+    "all the stars": "https://www.youtube.com/watch?v=JQbjS0_ZfJ0&list=RDJQbjS0_ZfJ0&start_radio=1&pp=ygUNYWxsIHRoZSBzdGFyc6AHAQ%3D%3D",
+    "starboy": "https://www.youtube.com/watch?v=34Na4j8AVgA&list=RD34Na4j8AVgA&start_radio=1&pp=ygUSc3RhcmJveSB0aGUgd2Vla25koAcB",
+    "after hours" : "https://www.youtube.com/watch?v=ygTZZpVkmKg&list=RDygTZZpVkmKg&start_radio=1&pp=ygULYWZ0ZXIgaG91cnOgBwE%3D",
+    "cry for me" : "https://www.youtube.com/watch?v=bn8gP5N8hqM&list=RDbn8gP5N8hqM&start_radio=1&pp=ygUKY3J5IGZvciBtZaAHAQ%3D%3D"
+}
